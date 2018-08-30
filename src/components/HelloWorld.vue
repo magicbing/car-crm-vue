78942220d0e71bbf1ba0203c7592859f7e3dcfee
magicbing <932201328@qq.com>
2018-08-31 00:42:21 +0800

    2

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,76 +1,27 @@
 <template>
 <el-container style="height: 700px; border: 1px solid #eee">
-  <!-- <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
-    <el-menu :default-openeds="['1', '3']">
-      <el-submenu index="1">
-        <template slot="title"><i class="el-icon-message"></i>导航一</template>
-        <el-menu-item-group>
-          <template slot="title">分组一</template>
-          <el-menu-item index="1-1">选项1</el-menu-item>
-          <el-menu-item index="1-2">选项2</el-menu-item>
-        </el-menu-item-group>
-        <el-menu-item-group title="分组2">
-          <el-menu-item index="1-3">选项3</el-menu-item>
-        </el-menu-item-group>
-        <el-submenu index="1-4">
-          <template slot="title">选项4</template>
-          <el-menu-item index="1-4-1">选项4-1</el-menu-item>
-        </el-submenu>
-      </el-submenu>
-      <el-submenu index="2">
-        <template slot="title"><i class="el-icon-menu"></i>导航二</template>
-        <el-menu-item-group>
-          <template slot="title">分组一</template>
-          <el-menu-item index="2-1">选项1</el-menu-item>
-          <el-menu-item index="2-2">选项2</el-menu-item>
-        </el-menu-item-group>
-        <el-menu-item-group title="分组2">
-          <el-menu-item index="2-3">选项3</el-menu-item>
-        </el-menu-item-group>
-        <el-submenu index="2-4">
-          <template slot="title">选项4</template>
-          <el-menu-item index="2-4-1">选项4-1</el-menu-item>
-        </el-submenu>
-      </el-submenu>
-      <el-submenu index="3">
-        <template slot="title"><i class="el-icon-setting"></i>导航三</template>
-        <el-menu-item-group>
-          <template slot="title">分组一</template>
-          <el-menu-item index="3-1">选项1</el-menu-item>
-          <el-menu-item index="3-2">选项2</el-menu-item>
-        </el-menu-item-group>
-        <el-menu-item-group title="分组2">
-          <el-menu-item index="3-3">选项3</el-menu-item>
-        </el-menu-item-group>
-        <el-submenu index="3-4">
-          <template slot="title">选项4</template>
-          <el-menu-item index="3-4-1">选项4-1</el-menu-item>
-        </el-submenu>
-      </el-submenu>
-    </el-menu>
-  </el-aside> -->
-  
+
   <el-container>
     <el-header style="text-align: right; font-size: 12px">
-      <el-dropdown>
-        <i class="el-icon-setting" style="margin-right: 15px"></i>
-        <el-dropdown-menu slot="dropdown">
-          <el-dropdown-item>查看</el-dropdown-item>
-          <el-dropdown-item>新增</el-dropdown-item>
-          <el-dropdown-item>删除</el-dropdown-item>
-        </el-dropdown-menu>
-      </el-dropdown>
-      <span>王小虎</span>
+      <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+        <el-menu-item index="1">查询</el-menu-item>
+        <el-menu-item index="2">新增</el-menu-item>
+        <el-menu-item index="3">详情</el-menu-item>
+        <!-- <el-menu-item index="4">修改日志</el-menu-item> -->
+      </el-menu>
     </el-header>
     
     <el-main>
       <el-table :data="tableData">
-        <el-table-column prop="date" label="日期" width="140">
-        </el-table-column>
-        <el-table-column prop="name" label="姓名" width="120">
-        </el-table-column>
-        <el-table-column prop="address" label="地址">
-        </el-table-column>
+        <el-table-column type="index" label="序号" width=""></el-table-column>
+        <el-table-column prop="phoneNo" label="手机号码" width=""></el-table-column>
+        <el-table-column prop="remark" label="备注" width=""></el-table-column>
+        <el-table-column prop="plateNo" label="车牌号" width=""></el-table-column>
+        <el-table-column prop="milage" label="公里数" width=""></el-table-column>
+        <el-table-column prop="wax" label="打蜡次数" width=""></el-table-column>
+        <el-table-column prop="wash" label="精洗次数" width=""></el-table-column>
+        <el-table-column prop="polish" label="抛光次数" width=""></el-table-column>
+        <el-table-column prop="disinfection" label="车内消读次数" width=""></el-table-column>
       </el-table>
     </el-main>
   </el-container>
@@ -83,15 +34,43 @@ export default {
   props: {
     msg: String
   },
+  methods: {
+    handleSelect(key, keyPath) {
+      /* eslint-disable */
+      console.log(key, keyPath);
+    },
+    queryAll() {
+      var that = this
+      fetch('http://localhost:3000/api/queryAll',{
+          method:'POST'
+        }).then(function(response) {
+          console.log(response)
+          return response.json();
+      }).then(function(data) {
+        console.log(data);
+        that.tableData = data
+      }).catch(function(e) {
+        console.log(e);
+      });
+    }
+  },
   data() {
     const item = {
       date: '2016-05-02',
       name: '王小虎',
       address: '上海市普陀区金沙江路 1518 弄'
     };
+    const two = [{"phoneNo":"13222225555q","remark":"123","plateNo":"测试2","milage":"2","wax":"1","wash":"1","polish":"119","disinfection":"69"},{"phoneNo":"18551649003","remark":"remark","plateNo":"苏A11111","milage":"111","wax":11156,"wash":22228,"polish":333338,"disinfection":123128}]
+    // let tableData
     return {
-      tableData: Array(20).fill(item)
+      tableData: [],
+      activeIndex: '1'
     }
+  },
+  created: function () {
+    console.log(this.activeIndex)
+    this.queryAll()
+    
   }
 }
 </script>
